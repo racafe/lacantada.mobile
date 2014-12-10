0e7be574fd85863d405fb6610fd06ba37e48746e
unknown <rafael.carrion@technit.com.mx>
2014-12-09 23:10:56 -0600

    cambios

--- js/index.js
+++ js/index.js
@@ -105,11 +105,8 @@ var app = {
 		$("#swiper").swipe( {
 			//Generic swipe handler for all directions
 			swipeRight:function(event, direction, distance, duration, fingerCount) {
-			  $('#showMenu').trigger(clickevent);
+			  $('#showMenu').trigger('touchstart');
 			  alert();
-			},
-			hold:function(event, target) {			  
-			  alert("You held the tap until the longTapthreshold was reached" );
 			},
 			//Default is 75px, set to 0 for demo so any distance triggers swipe
 			threshold:50
