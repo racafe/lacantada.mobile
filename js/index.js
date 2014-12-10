da9344ed023859c9f50627138c02d941806bc1f0
unknown <rafael.carrion@technit.com.mx>
2014-12-09 23:06:34 -0600

    cambios

--- js/index.js
+++ js/index.js
@@ -106,9 +106,13 @@ var app = {
 			//Generic swipe handler for all directions
 			swipeRight:function(event, direction, distance, duration, fingerCount) {
 			  $('#showMenu').trigger(clickevent);
+			  alert();
+			},
+			hold:function(event, target) {			  
+			  alert("You held the tap until the longTapthreshold was reached" );
 			},
 			//Default is 75px, set to 0 for demo so any distance triggers swipe
-			threshold:0
+			threshold:50
 		  });
 		slides = $('#slides').bxSlider({
 			controls: false,
